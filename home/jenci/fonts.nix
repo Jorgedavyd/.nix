9c3d295195e8047bbfb31cd27d5ba8a3bfb018bc
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-09 01:58:10 -0300

    setting up the identation

--- home/jenci/fonts.nix
+++ home/jenci/fonts.nix
@@ -1,25 +1,25 @@
 { pkgs, lib, ... }:
 
 let
-  fontFromURL = name: hash: pkgs.stdenv.mkDerivation {
-    pname = name;
-    version = "3.4.0";
-    src = pkgs.fetchzip {
-      url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/${name}.zip";
-      hash = hash;
-    };
+    fontFromURL = name: hash: pkgs.stdenv.mkDerivation {
+        pname = name;
+        version = "3.4.0";
+        src = pkgs.fetchzip {
+            url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/${name}.zip";
+            hash = hash;
+        };
 
-    installPhase = ''
+        installPhase = ''
       mkdir -p $out/share/fonts/${name}
       cp -r * $out/share/fonts/${name}
-    '';
-  };
+        '';
+    };
 
-  iosevka = fontFromURL "Iosevka" "0bz0jykmb5k9bippxz7hqz12iw772w9ik2ki1k8w6g4kiyxyxisi";
-  jetbrains = fontFromURL "JetBrainsMono" "0g29gj9d6720grfr2vasnvdppzw4hycpfyd5di54d2p4mkrmzw3n";
-  commit = fontFromURL "CommitMono" "08vzlrx5wdz1czifrmjv5nl68fiq01ki8nb4xa53j153ar08qrgs";
+    iosevka = fontFromURL "Iosevka" "0bz0jykmb5k9bippxz7hqz12iw772w9ik2ki1k8w6g4kiyxyxisi";
+    jetbrains = fontFromURL "JetBrainsMono" "0g29gj9d6720grfr2vasnvdppzw4hycpfyd5di54d2p4mkrmzw3n";
+    commit = fontFromURL "CommitMono" "08vzlrx5wdz1czifrmjv5nl68fiq01ki8nb4xa53j153ar08qrgs";
 
 in {
-  fonts.fontconfig.enable = true;
-  home.packages = [ iosevka jetbrains commit ];
+    fonts.fontconfig.enable = true;
+    home.packages = [ iosevka jetbrains commit ];
 }
