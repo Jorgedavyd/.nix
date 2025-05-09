f13ccf6241e5d0c33274dd4b5a15c3b4f5bb4c9b
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-09 17:52:36 -0300

    default

--- home/jenci/theme.nix
+++ home/jenci/theme.nix
@@ -30,7 +30,7 @@
 
     qt = {
         enable = true;
-        platformTheme = "gtk";
+        platformTheme.name = "gtk";
         style = {
             name = "kvantum";
             package = pkgs.libsForQt5.qtstyleplugin-kvantum;
