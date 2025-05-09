4dbfc732f7a6186975c60783a6f35082d84fe500
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-09 00:45:17 -0300

    default

--- hosts/main/cuda.nix
+++ hosts/main/cuda.nix
@@ -8,6 +8,7 @@ cudapackages,
     environment.systemPackages = with pkgs; [
         paraview  ## modeling
         # cudapackages.nsight_systems cudapackages.nsight_compute cudapackages.nsight_vse
-        cudapackages.tensorrt cudapackages.cudnn cudapackages.cudatoolkit ## base deep learning and cuda
+        # cudapackages.tensorrt
+        cudapackages.cudnn cudapackages.cudatoolkit ## base deep learning and cuda
     ];
 }
