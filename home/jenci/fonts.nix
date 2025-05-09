38dc0c699aed5c26b1f83c9272417ae8411ce3d5
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-09 18:03:35 -0300

    default

--- home/jenci/fonts.nix
+++ home/jenci/fonts.nix
@@ -14,9 +14,9 @@ let
         '';
     };
 
-    iosevka = fontFromURL "Iosevka" "sha256-0bz0jykmb5k9bippxz7hqz12iw772w9ik2ki1k8w6g4kiyxyxisi=";
-    jetbrains = fontFromURL "JetBrainsMono" "sha256-0g29gj9d6720grfr2vasnvdppzw4hycpfyd5di54d2p4mkrmzw3n=";
-    commit = fontFromURL "CommitMono" "sha256-08vzlrx5wdz1czifrmjv5nl68fiq01ki8nb4xa53j153ar08qrgs=";
+    iosevka = fontFromURL "Iosevka" "0bz0jykmb5k9bippxz7hqz12iw772w9ik2ki1k8w6g4kiyxyxisi";
+    jetbrains = fontFromURL "JetBrainsMono" "0g29gj9d6720grfr2vasnvdppzw4hycpfyd5di54d2p4mkrmzw3n";
+    commit = fontFromURL "CommitMono" "08vzlrx5wdz1czifrmjv5nl68fiq01ki8nb4xa53j153ar08qrgs";
 
 in {
     fonts.fontconfig.enable = true;
--- home/jenci/swww.nix
+++ home/jenci/swww.nix
@@ -2,12 +2,12 @@
 
 let
     wallpaperSpec = [
-        { url = "https://github.com/HomeomorphicHooligan/arch-minimal-wallpapers/blob/main/wallpapers/full-hd/rosepine.png?raw=true"; sha256 = "sha256-09nxpgskcnma8hhhzx5hlz51n7dgdxnls1vc8sphn2l59c479812="; }
-        { url = "https://raw.githubusercontent.com/DenverCoder1/minimalistic-wallpaper-collection/main/images/alena-aenami-horizon.png"; sha256 = "sha256-18nhww2vcly7gzs5azz9l2260ygakmrqi8p480lzhq2w9jg1k7f9="; }
-        { url = "https://raw.githubusercontent.com/DenverCoder1/minimalistic-wallpaper-collection/main/images/colormate-monogatari.jpg"; sha256 = "sha256-12271bd70hgmmjca564y8j4j6xx3qlra42dnpns6xhh52cdfv2w6="; }
-        { url = "https://raw.githubusercontent.com/DenverCoder1/minimalistic-wallpaper-collection/main/images/darkkal44-come-with-me-surfaces.png"; sha256 = "sha256-15pznc75y3131d6bwqhq289sd8pccgyp4rg1kx5pv41lzcfjh8r9="; }
-        { url = "https://raw.githubusercontent.com/DenverCoder1/minimalistic-wallpaper-collection/main/images/gloomilygray-journey.jpg"; sha256 = "sha256-07cs3gl8r059xja2rfm5qpckyb21s5sw29vq9fvgyln462zs1vqx="; }
-        { url = "https://raw.githubusercontent.com/DenverCoder1/minimalistic-wallpaper-collection/main/images/alena-aenami-in-search-of-peace.png"; sha256 = "sha256-0p8hn43naix2pb4pdbw1jdbz4dmrj270a3w5llqwajk1rqjh22mx="; }
+        { url = "https://github.com/HomeomorphicHooligan/arch-minimal-wallpapers/blob/main/wallpapers/full-hd/rosepine.png?raw=true"; sha256 = "09nxpgskcnma8hhhzx5hlz51n7dgdxnls1vc8sphn2l59c479812"; }
+        { url = "https://raw.githubusercontent.com/DenverCoder1/minimalistic-wallpaper-collection/main/images/alena-aenami-horizon.png"; sha256 = "18nhww2vcly7gzs5azz9l2260ygakmrqi8p480lzhq2w9jg1k7f9"; }
+        { url = "https://raw.githubusercontent.com/DenverCoder1/minimalistic-wallpaper-collection/main/images/colormate-monogatari.jpg"; sha256 = "12271bd70hgmmjca564y8j4j6xx3qlra42dnpns6xhh52cdfv2w6"; }
+        { url = "https://raw.githubusercontent.com/DenverCoder1/minimalistic-wallpaper-collection/main/images/darkkal44-come-with-me-surfaces.png"; sha256 = "15pznc75y3131d6bwqhq289sd8pccgyp4rg1kx5pv41lzcfjh8r9"; }
+        { url = "https://raw.githubusercontent.com/DenverCoder1/minimalistic-wallpaper-collection/main/images/gloomilygray-journey.jpg"; sha256 = "07cs3gl8r059xja2rfm5qpckyb21s5sw29vq9fvgyln462zs1vqx"; }
+        { url = "https://raw.githubusercontent.com/DenverCoder1/minimalistic-wallpaper-collection/main/images/alena-aenami-in-search-of-peace.png"; sha256 = "0p8hn43naix2pb4pdbw1jdbz4dmrj270a3w5llqwajk1rqjh22mx"; }
     ];
     basename = url:
         let
