c0f22291c09187e6b36eb600ea26e68df0eeb199
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-09 17:59:14 -0300

    changed hashes

--- home/jenci/fonts.nix
+++ home/jenci/fonts.nix
@@ -5,7 +5,7 @@ let
         version = "3.4.0";
         src = pkgs.fetchzip {
             url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/${name}.zip";
-            sha256 = sha256;
+            hash = sha256;
         };
 
         installPhase = ''
@@ -14,9 +14,9 @@ let
         '';
     };
 
-    iosevka = fontFromURL "Iosevka" "0bz0jykmb5k9bippxz7hqz12iw772w9ik2ki1k8w6g4kiyxyxisi";
-    jetbrains = fontFromURL "JetBrainsMono" "0g29gj9d6720grfr2vasnvdppzw4hycpfyd5di54d2p4mkrmzw3n";
-    commit = fontFromURL "CommitMono" "08vzlrx5wdz1czifrmjv5nl68fiq01ki8nb4xa53j153ar08qrgs";
+    iosevka = fontFromURL "Iosevka" "sha256-0bz0jykmb5k9bippxz7hqz12iw772w9ik2ki1k8w6g4kiyxyxisi=";
+    jetbrains = fontFromURL "JetBrainsMono" "sha256-0g29gj9d6720grfr2vasnvdppzw4hycpfyd5di54d2p4mkrmzw3n=";
+    commit = fontFromURL "CommitMono" "sha256-08vzlrx5wdz1czifrmjv5nl68fiq01ki8nb4xa53j153ar08qrgs=";
 
 in {
     fonts.fontconfig.enable = true;
