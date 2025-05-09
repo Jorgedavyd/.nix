9ec9f8ab5369c804b7bea8a24a72f91323ff37cf
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-09 18:14:24 -0300

    solved commitmono

--- home/jenci/fonts.nix
+++ home/jenci/fonts.nix
@@ -16,8 +16,20 @@ let
 
     iosevka = fontFromURL "Iosevka" "0bz0jykmb5k9bippxz7hqz12iw772w9ik2ki1k8w6g4kiyxyxisi";
     jetbrains = fontFromURL "JetBrainsMono" "0g29gj9d6720grfr2vasnvdppzw4hycpfyd5di54d2p4mkrmzw3n";
-    commit = fontFromURL "CommitMono" "08vzlrx5wdz1czifrmjv5nl68fiq01ki8nb4xa53j153ar08qrgs";
 
+    commit = pkgs.stdenv.mkDerivation {
+        pname = name;
+        version = "3.4.0";
+        src = pkgs.fetchzip {
+            url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/CommitMono.zip";
+            sha256 = "08vzlrx5wdz1czifrmjv5nl68fiq01ki8nb4xa53j153ar08qrgs";
+        };
+
+        installPhase = ''
+      mkdir -p $out/share/fonts/CommitMono
+      cp -r * $out/share/fonts/CommitMono
+        '';
+    };
 in {
     fonts.fontconfig.enable = true;
     home.packages = [ iosevka jetbrains commit ];
