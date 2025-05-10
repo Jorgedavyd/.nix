e8dfb59faa68f6692e842a11131be8f9cbd326df
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-10 17:51:36 -0300

    default

--- hosts/laptop/networking.nix
+++ hosts/laptop/networking.nix
@@ -6,11 +6,11 @@
     networking = {
         interfaces = {
             wlan0.ipv4.addresses = [{
-                address = "192.168.100.10";
+                address = "192.168.100.100";
                 prefixLength = 24;
             }];
             eth0.ipv4.addresses = [{
-                address = "192.168.100.20";
+                address = "192.168.100.40";
                 prefixLength = 24;
             }];
         };
--- hosts/main/networking.nix
+++ hosts/main/networking.nix
@@ -6,7 +6,7 @@
 {
   networking = {
     interfaces.eth0.ipv4.addresses = [{
-      address = "192.168.100.100";
+      address = "192.168.100.200";
       prefixLength = 24;
     }];
   };
