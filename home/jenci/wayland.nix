c9c7a736937539206467036a9bcb3102b469c653
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-10 20:21:16 -0300

    default

--- home/jenci/wayland.nix
+++ home/jenci/wayland.nix
@@ -1,10 +1,5 @@
 {config, pkgs, ... }:
 {
-    security.pam.services.hyrplock = {
-        text = ''
-            auth include login
-            '';
-    };
     wayland.windowManager.hyprland = {
         enable = true;
         package = pkgs.hyprland;
