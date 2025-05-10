94056b8cc210ec1f52645ae3991ee7e3832a8fcd
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-10 14:04:00 -0300

    drop commit

--- home/jenci/fonts.nix
+++ home/jenci/fonts.nix
@@ -17,21 +17,8 @@ let
     iosevka = fontFromURL "Iosevka" "0bz0jykmb5k9bippxz7hqz12iw772w9ik2ki1k8w6g4kiyxyxisi";
     jetbrains = fontFromURL "JetBrainsMono" "0g29gj9d6720grfr2vasnvdppzw4hycpfyd5di54d2p4mkrmzw3n";
 
-    commit = pkgs.stdenv.mkDerivation {
-        pname = "CommitMono";
-        version = "3.4.0";
-        src = pkgs.fetchzip {
-            url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/CommitMono.zip";
-            sha256 = "08vzlrx5wdz1czifrmjv5nl68fiq01ki8nb4xa53j153ar08qrgs";
-            stripRoot = false;
-        };
-
-        installPhase = ''
-      mkdir -p $out/share/fonts/CommitMono
-      cp -r * $out/share/fonts/CommitMono
-        '';
     };
 in {
     fonts.fontconfig.enable = true;
-    home.packages = [ iosevka jetbrains commit ];
+    home.packages = [ iosevka jetbrains ];
 }
