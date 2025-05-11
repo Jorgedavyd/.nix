7ca239a99a5bb97dd6e5d80450346726b62292bf
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-11 18:17:00 -0300

    default

--- hosts/common/hardware.nix
+++ hosts/common/hardware.nix
@@ -6,7 +6,6 @@ lib,
 }:
 
 {
-    imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
     hardware.pulseaudio.enable = false;
     hardware.graphics = {
