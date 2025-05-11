3e99e98dddbc6fecdcc75deb8067fc4e69f5ad5e
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-11 16:43:02 -0300

    default

--- hosts/main/hardware.nix
+++ hosts/main/hardware.nix
@@ -22,8 +22,8 @@ lib,
     };
     boot.extraModprobeConfig = ''
     options nvidia-drm modeset=1
+    options nvidia-drm fbdev=1
     '';
-    # options nvidia-drm fbdev=1
     # options nvidia NVreg_UsePageAttributeTable=1
     # options nvidia NVreg_PreserveVideoMemoryAllocations=1
     # options nvidia NVreg_TemporaryFilePath=/var/tmp
