b23f3f2ee7bcbfd71163b5f6b4f6960f267e6e78
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-11 01:08:20 -0300

    default

--- home/jenci/theme.nix
+++ home/jenci/theme.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
     home.pointerCursor = {
+        name = "rose-pine-cursor";
         gtk.enable = true;
         size = 24;
         package = pkgs.rose-pine-cursor;
@@ -15,8 +16,10 @@
         iconTheme = {
             package = pkgs.rose-pine-icon-theme;
             name = "rose-pine";
+        };
         theme = {
             package = pkgs.rose-pine-gtk-theme;
+            name = "rose-pine";
         };
     };
 
