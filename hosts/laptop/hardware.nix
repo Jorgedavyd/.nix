7de9a268803130ebaf1662f17da49117db209eaf
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-10 23:41:55 -0300

    default

--- hosts/laptop/hardware.nix
+++ hosts/laptop/hardware.nix
@@ -6,6 +6,7 @@ lib,
 }:
 
 {
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
     boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
     boot.initrd.kernelModules = [ ];
     boot.kernelModules = [ "kvm-intel" ];
--- hosts/main/hardware.nix
+++ hosts/main/hardware.nix
@@ -5,6 +5,7 @@ lib,
 }:
 
 {
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
     boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
     boot.initrd.kernelModules = [ ];
     boot.initrd.systemd.enable = true;
