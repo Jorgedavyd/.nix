cd04e52db07bf6edcc56e3dfe2a8e1c4548e2b96
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-10 23:49:42 -0300

    default

--- home/jenci/xdg.nix
+++ home/jenci/xdg.nix
@@ -32,5 +32,16 @@ in {
                 XDG_PROJECTS_DIR = "${homedirectory}/projects";
             };
         };
+        portal = {
+            enable = true;
+            extraPortals = with pkgs; [
+                xdg-desktop-portal-hyprland
+            ];
+            config = {
+                common = {
+                    default = "hyprland";
+                };
+            };
+        };
     };
 }
