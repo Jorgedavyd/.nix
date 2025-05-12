6540d933972d237dc6ebdf5fbcfb37a8d3701203
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-12 18:51:40 -0300

    no dm

--- hosts/main/services.nix
+++ hosts/main/services.nix
@@ -1,11 +1,5 @@
 _: {
     services.xserver = {
         videoDrivers = [ "nvidia" ];
-        displayManager = {
-            gdm = {
-                enable = true;
-                wayland = true;
-            };
-        };
     };
 }
