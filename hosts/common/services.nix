e8224473ec58e0f49fb2d0f5bcee057f03146375
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-13 14:40:12 -0300

    onedrive setup

--- hosts/common/services.nix
+++ hosts/common/services.nix
@@ -1,31 +1,37 @@
-{ pkgs, ... }: {
-    services = {
-        pipewire = {
-            enable = true;
-            alsa.enable = true;
-            alsa.support32Bit = true;
-            pulse.enable = true;
-        };
-        openssh.enable = true;
-        seatd.enable = true;
-        dbus.enable = true;
-        gnome.gnome-keyring.enable = true;
-        gnome.glib-networking.enable = true;
-    };
-    systemd.user.services.onedrive = {
-	    Unit = {
-		    Description = "OneDrive cloud sync service";
-		    After = [ "network.target" ];
-	    };
-
-	    Service = {
-		    ExecStart = "${pkgs.onedrive}/bin/onedrive --monitor --resync";
-		    Restart = "on-failure";
-	    };
-
-	    Install = {
-		    WantedBy = [ "default.target" ];
-	    };
-    };
-
+{ pkgs, username, ... }: 
+{
+	services = {
+		pipewire = {
+			enable = true;
+			alsa.enable = true;
+			alsa.support32Bit = true;
+			pulse.enable = true;
+		};
+		openssh.enable = true;
+		seatd.enable = true;
+		dbus.enable = true;
+		gnome.gnome-keyring.enable = true;
+		gnome.glib-networking.enable = true;
+	};
+	systemd.user.services.onedrive = {
+		enable = true;
+		unitConfig = {
+			Description = "OneDrive client for Linux";
+			RequiresMountFor = "/data";
+		};
+		serviceConfig = {
+			ProtectSystem = "full";
+			ProtectHostname = true;
+			ProtectKernelTunables = true;
+			ProtectControlGroups = true;
+			RestrictRealtime = true;
+			Group = "users";
+			ExecStart= ''${pkgs.onedrive}/bin/onedrive --monitor --syncdir /data/OneDrive'';
+			Restart = "on-failure";
+			RestartSec = 3;
+			RestartPreventExitStatus = 126;
+			TimeoutStopSec = 90;
+		};
+		wantedBy = [ "default.target" ];
+	};
 }
