812dbccfa94c818175c0c0b85dc4ee9986ff37fd
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-12 23:37:18 -0300

    default

--- hosts/common/packages.nix
+++ hosts/common/packages.nix
@@ -9,4 +9,16 @@ pkgs,
         hyprland libsForQt5.qt5.qtwayland libsForQt5.qt5ct libva
         xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
     ];
+    dconf.settings = {
+        "org/gnome/desktop/interface" = {
+            cursor-theme = "Bibata-Modern-Ice";
+            cursor-size = 20;
+            font-name = "JetBrainsMono Nerd Font 9";
+            document-font-name = "JetBrainsMono Nerd Font 9";
+            monospace-font-name = "JetBrainsMono Nerd Font 9";
+            font-antialiasing = "rgba";
+            font-hinting = "full";
+            color-scheme = "prefer-dark";
+        };
+    };
 }
