c04f4fa6a2108fc8acc8e16aa8cf40a6a674d407
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-15 01:20:33 -0300

    default

--- home/jenci/nvim/lua/jenci/lazy/latex.lua
+++ home/jenci/nvim/lua/jenci/lazy/latex.lua
@@ -4,16 +4,13 @@ return {
         "L3MON4D3/LuaSnip",
         "lervag/vimtex",
     },
-    keys = {
-        { '<leader>ll', ':VimtexCompile<CR>' },
-        { '<leader>lv', ':VimtexView<CR>' },
-
-    },
     config = function()
-        require("luasnip-latex-snippets").setup()
-        require("luasnip").config.setup { enable_autosnippets = true }
         vim.g.vimtex_view_general_viewer = 'zathura'
         vim.g.vimtex_compiler_method = 'latexmk'
         vim.g.vimtex_view_method = 'general'
-    end,
+        vim.keymap.set('n', '<leader>ll', ':VimtexCompile<CR>', { noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>lv', ':VimtexView<CR>', { noremap = true, silent = true })
+        require("luasnip-latex-snippets").setup()
+        require("luasnip").config.setup { enable_autosnippets = true }
+    end
 }
