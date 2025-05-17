37e38b996c1e36678c4361f315fccc29da56fdde
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-17 18:00:59 -0300

    setup waybar theme, hyprland and environment variables for nvidia

--- home/jenci/nvim/lua/jenci/lazy/treesitter.lua
+++ home/jenci/nvim/lua/jenci/lazy/treesitter.lua
@@ -1,22 +1,22 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-        ensure_installed = {
-            "python", "c", "lua", "cuda", "cpp", "zig",
-            "rust", "go", "java", "sql", "cmake", "bash",
-            "markdown_inline", "vimdoc", "vim", "toml",
-            "csv", "json", "make", "dockerfile", "xml", "yaml",
-            "rego"
-        },
-        sync_install = true,
-        auto_install = true,
-        indent = {
-            enable = true
-        },
-        highlight = {
-            enable = true,
-            disable = {"latex"},
-            additional_vim_regex_highlighting = false,
-        }
-    }
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "python", "c", "lua", "cuda", "cpp", "zig",
+                "rust", "java", "sql", "cmake", "bash",
+                "markdown_inline", "vimdoc", "vim", "toml",
+                "csv", "json", "make", "dockerfile", "xml", "yaml",
+                "rego"
+            },
+            sync_install = true,
+            auto_install = true,
+            indent = { enable = true },
+            highlight = {
+                enable = true,
+                disable = {"latex"},
+                additional_vim_regex_highlighting = false,
+            }
+        })
+    end
 }
--- todo.md
+++ todo.md
@@ -1,5 +1,15 @@
 # Tasks
 
+## Workflow tasks
+- [ ] Setup waybar for occasions@waybar
+- [ ] Set up a good enough theme that is overall better@nvim
+- [ ] Minimize lualne@nvim
+- [ ] Drop swww@swww
+- [ ] Performance engineering in hyprland (drop decorations and borders)@hypr
+- [ ] Solve windowrule bug@hypr
+
+## Nix Tasks (also workflow)
+
 - [ ] Learn how to use flakes throughly for reproducibility.
 - [ ] Create overlays for particular packages I use.
     - [ ] cudnn, tensorboard, cufft, etc.
