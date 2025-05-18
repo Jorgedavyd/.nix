dc11fcc7867040ab2474e8e235e8bb6283e75a17
Jorgedavyd <jorged.encyso@gmail.com>
2025-05-18 10:31:08 -0300

    setting up special packages

--- overlays/cuda/default.nix
+++
@@ -1,5 +0,0 @@
-final: prev: {
-    nsight_systems = final.callPackage ./cuda/nsight_systems.nix {};
-    nsight_compute = final.callPackage ./cuda/nsight_compute.nix {};
-    cudnn = final.callPackage ./cuda/cudnn.nix {};
-}
