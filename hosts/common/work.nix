951cfdb2c6f79c2708b85e550b4255a591a3185c
Jorgedavyd <jorged.encyso@gmail.com>
2025-06-02 08:44:45 -0300

    without service

--- hosts/common/work.nix
+++ hosts/common/work.nix
@@ -1,4 +1,3 @@
 { pkgs, ... }: {
-    environment.systemPackages = with pkgs; [ teams-for-linux ];
-    services.openvpn.servers.officeVPN  = { config = '' config /root/nixos/openvpn/officeVPN.conf ''; };
+    environment.systemPackages = with pkgs; [ teams-for-linux openvpn ];
 }
