53e48f3dbfaba783102062dcccb6b8953ce61ae0
Jorgedavyd <jorged.encyso@gmail.com>
2025-06-21 17:28:21 -0300

    default

--- home/jenci/nvim/lua/jenci/lazy/ui/theme.lua
+++ home/jenci/nvim/lua/jenci/lazy/ui/theme.lua
@@ -1,7 +1,35 @@
 return {
     "vague2k/vague.nvim",
     config = function()
-        require("vague").setup({ })
+        require("vague").setup({
+            transparent = true,
+            bold = true,
+            italic = false,
+            style = {
+                boolean = "bold",
+                number = "none",
+                float = "none",
+                error = "bold",
+                comments = "none",
+                conditionals = "none",
+                functions = "none",
+                headings = "bold",
+                operators = "none",
+                strings = "none",
+                variables = "none",
+
+                keywords = "none",
+                keyword_return = "none",
+                keywords_loop = "none",
+                keywords_label = "none",
+                keywords_exception = "none",
+
+                builtin_constants = "bold",
+                builtin_functions = "none",
+                builtin_types = "bold",
+                builtin_variables = "none",
+            }
+        })
         vim.cmd("colorscheme vague")
     end
 }
