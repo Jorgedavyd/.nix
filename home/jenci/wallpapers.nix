3eadf5d0e61f9ab56161ba6d616911982cb54ad5
Jorgedavyd <jorged.encyso@gmail.com>
2025-12-07 21:10:27 -0300

    default

--- home/jenci/wallpapers.nix
+++ home/jenci/wallpapers.nix
@@ -3,8 +3,20 @@
 let
     wallpaperSpec = [
         {
-            url = "https://raw.githubusercontent.com/FrenzyExists/wallpapers/main/Gruv/nix-gruv.jpg";
-            sha256 = "sha256-jP+J4CC5OcMgyYIStw0ieOvW9yBGJSLtYpPW6cL4v0g=";
+            url = "https://raw.githubusercontent.com/rose-pine/wallpapers/refs/heads/main/MC_dark_house_rain.png";
+            sha256 = "sha256-PHVdefYSAhMHeejDzuZsIQId57PrOaAyPri4fCpX7Eg=";
+        }
+        {
+            url = "https://raw.githubusercontent.com/rose-pine/wallpapers/main/MC_cave_sunset_view.png";
+            sha256 = "sha256-aMVFkJPW06nPZSPPMykeZcmVF27YQBNhAUV7PMdAGo4=";
+        }
+        {
+            url = "https://raw.githubusercontent.com/rose-pine/wallpapers/refs/heads/main/min-linux.jpg";
+            sha256 = "sha256-OW8HujRALZ4jmJD9XGJIOc3IjRpFPmUzXY0XMsNIhCg=";
+        }
+        {
+            url = "https://raw.githubusercontent.com/rose-pine/wallpapers/refs/heads/main/Arch.png";
+            sha256 = "sha256-skyY8GjHuZ2sU0YfnSxKvfvB/96EuZ7bPx+ke1jA2Fw=";
         }
     ];
 
